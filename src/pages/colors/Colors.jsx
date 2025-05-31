8fb5f52e2738b03e0b41f8e75cc7621c52701570
koshtijatin26 <koshtijatin26@gmail.com>
2025-05-31 18:44:41 +0530

    general

--- src/pages/colors/Colors.jsx
+++ src/pages/colors/Colors.jsx
@@ -186,7 +186,6 @@ const Colors = () => {
               colors.map((cat, index) => (
                 <tr key={cat._id}>
                   <td>{index + 1 + (currentPage - 1) * pageSize}</td>
-                  {/* <td>{index + 1}</td> */}
                   <td>
                       {cat.name}
                   </td>
